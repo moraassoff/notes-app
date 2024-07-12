698c3c07a25d6feb5541f2d81f671e85f8c80e71
moraassoff <moraassoff.scholar@gmail.com>
2024-07-11 19:07:41 -0600

    primer commit, inicializacion proyecto

---
+++ app.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+let notes = [];
+
+//funcionalidades servidor
+// navegacion y apis
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+//
+app.get('/notas', (req, res) => {
+  res.json(notes);
+});
+
+app.get('/notas/:id', (req, res) => {
+  const note = notes.find(note => note.id === req.params.id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).send('Nota no encontrada');
+  }
+});
+
+app.post('/notas', (req, res) => {
+  const { title, content, tags } = req.body;
+
+  if (!title || title.trim() === '' || !content || content.trim() === '') {
+    return res.status(400).send('El título y el contenido son obligatorios para crearl la nota');
+  }
+
+  const newNote = {
+    id: uuidv4(),
+    title: title.trim(),
+    content: content.trim(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    tags: tags ? tags.split(',').map(tag => tag.trim()).filter(tag => tag !== '') : []
+  };
+
+  notes.push(newNote);
+  res.status(201).json(newNote);
+});
+
+app.put('/notas/:id', (req, res) => {
+  const { title, content, tags } = req.body;
+
+  if (!title || title.trim() === '' || !content || content.trim() === '') {
+    return res.status(400).send('El título y el contenido son obligatorios');
+  }
+
+  const note = notes.find(note => note.id === req.params.id);
+  if (note) {
+    note.title = title.trim();
+    note.content = content.trim();
+    note.updatedAt = new Date();
+    note.tags = tags ? tags.split(',').map(tag => tag.trim()).filter(tag => tag !== '') : [];
+    res.json(note);
+  } else {
+    res.status(404).send('Nota no encontrada');
+  }
+});
+
+app.delete('/notas/:id', (req, res) => {
+  notes = notes.filter(note => note.id !== req.params.id);
+  res.status(204).send();
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
